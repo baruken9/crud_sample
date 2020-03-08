f23e612dd4d1c85127ca2c4ba93927fc970388ba
桃原健太 <>
2020-03-08 19:54:25 +0900

    Rails課題２

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+  #newアクション
+  def new
+    @user = User.new
+  end
+  #createアクション
+  def create
+    #Strong Paramenterの記述
+    User.create(user_params)
+    redirect_to :action => "index"
+  end
+  #editアクション
+  def edit
+    @user = User.find(params[:id])
+  end
+  #updateアクション
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to :action => "index"
+  end
+  #deleteアクション
+  def destroy
+    user = User.find(params[:id])
+    user.delete
+    redirect_to :action => "index"
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :age)
+  end
+end
